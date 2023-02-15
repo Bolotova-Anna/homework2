9e6b023f243a1705d056cfedd6589e504d519720
Anna Bolotova <anechkamilaha69@gmail.com>
2023-02-15 22:15:45 +0300

    error correction

--- task10/Program.cs
+++ task10/Program.cs
@@ -2,8 +2,14 @@
 // и на выходе показывает вторую цифру этого числа.
 System.Console.WriteLine("введите трехзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
-int temperarynumber = number/10;
-System.Console.WriteLine(temperarynumber%10);
-
+if (number>=100 && number<=999)
+{
+    int temperarynumber = number/10;
+    System.Console.WriteLine(temperarynumber%10);
+}
+else
+{
+    System.Console.WriteLine("неверно. необходимо было ввести трехзначное число");
+}
 
 
