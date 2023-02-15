28ea204b43b9aeef3346eaaf277eb78ee5bf3b1c
Anna Bolotova <anechkamilaha69@gmail.com>
2023-02-15 20:16:02 +0300

    was done task10

---
+++ task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+System.Console.WriteLine("введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int temperarynumber = number/10;
+System.Console.WriteLine(temperarynumber%10);
+
+
+
