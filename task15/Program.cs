9d6cf66a5b588ef0188d4ab237488ed4a1677183
Anna Bolotova <anechkamilaha69@gmail.com>
2023-02-15 22:25:03 +0300

    error correction

--- task15/Program.cs
+++ task15/Program.cs
@@ -2,31 +2,15 @@
 // и проверяет, является ли этот день выходным.
 Console.WriteLine("введите число от 1 до 7");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number == 1) 
-{
-    System.Console.WriteLine("нет");
-}
-else if (number == 2) 
-{
-    System.Console.WriteLine("нет");
-}
-else if (number == 3) 
-{
-    System.Console.WriteLine("нет");
-}
-else if (number == 4) 
-{
-    System.Console.WriteLine("нет");
-}
-else if (number == 5) 
-{
-    System.Console.WriteLine("нет");
-}
-else if (number == 6) 
+if (number == 6 || number == 7) 
 {
     System.Console.WriteLine("да");
 }
-else if (number == 7)
+else if (number >=1 && number <=5) 
 {
-    System.Console.WriteLine("да");
+    System.Console.WriteLine("нет");
+}
+else 
+{
+    System.Console.WriteLine("такого дня недели не существует");
 }
